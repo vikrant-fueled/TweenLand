a4521cf7a7cc9a8016fd3a79d42b12e1359d4cc6
Vikrant Pogula <vtp@fueled.co>
2013-07-25 16:44:45 +0530

    Input working

--- scripts.js
+++ scripts.js
@@ -1,3 +1,15 @@
 function _initialize(){
-	$('#output').html("Hello, World!");
+	$('#output').html("Hello from Tweenland!");
+
+
+	var _InputFILE = 'tweens600.txt';
+
+	var _TweenARRAY = new Array();
+	var _TweenARRAY = loadFileIntoArray(_InputFILE);
+
+	var _Result = new Array();
+
+	
+
+	console.log(_TweenARRAY);
 }
