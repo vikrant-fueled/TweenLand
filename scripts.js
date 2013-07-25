f8bea2cb36990d0b864352e26b59cd6ff199ce8a
Vikrant Pogula <vtp@fueled.co>
2013-07-25 17:32:14 +0530

    Approach 1:
    Select a random Tween and create its clone.
    Search for this clone in Tweens.
    
    FAIL !

--- scripts.js
+++ scripts.js
@@ -7,9 +7,42 @@ function _initialize(){
 	var _TweenARRAY = new Array();
 	var _TweenARRAY = loadFileIntoArray(_InputFILE);
 
+	var _ChildARRAY = new Array();
+
 	var _Result = new Array();
+
+	var _Integers = new Array();
+	_Integers = integerArray(_TweenARRAY.length);
+
+	//SELECT A RANDOM TWEEN FROM SAMPLE SPACE AND ASSUME ITS THE SUPREME PARENT
 
 	
 
-	console.log(_TweenARRAY);
+	var _CurrentPARENT = Math.floor(Math.random() * _TweenARRAY.length);
+	var _CHILD = _CreateCLONE(_TweenARRAY[_CurrentPARENT]);
+
+	var _LOCATION = _TweenARRAY.indexOf(_CHILD);
+
+	console.log(_LOCATION);
+}
+
+
+function _CreateCLONE(_PARENT){
+
+	var _CHILD = new Array();
+	var _PARENT = _PARENT.split('');
+
+	var _LENGTH = _PARENT.length;
+
+	for (var i = 0; i < _LENGTH; i++) {
+
+		if(Math.random() <= 0.8)
+		_CHILD[i] = _PARENT[i];
+		else
+		_CHILD[i] = (1 - _PARENT[i]).toString();
+	}
+
+	var _CLONE = _CHILD.join('');
+
+	return _CLONE;
 }
